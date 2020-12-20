e7303b67efaefd7f996488e47c1997c358723a28
NoMeNoMeMe <mateusz.witkowski0000@gmail.com>
2020-12-20 14:09:30 +0100

    Added pagination for home page and ability to filter posts by user

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,8 @@ from .views import (
     PostDetailView,
     PostCreateView,
     PostUpdateView,
-    PostDeleteView
+    PostDeleteView,
+    UserPostListView
 )
 from django.urls import path
 from . import views  # . is for current directory
@@ -11,10 +12,11 @@ from . import views  # . is for current directory
 # we are already in blog/ so only next part is included
 urlpatterns = [
     # pk for primary key, int for integers only
+    path('', PostListView.as_view(), name='blog-home'),
+    path('about/', views.about, name='blog-about'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
-    path('', PostListView.as_view(), name='blog-home'),
-    path('about/', views.about, name='blog-about'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-post'),
 ]
